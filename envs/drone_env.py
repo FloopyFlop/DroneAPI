0a10d48a7539a6be10d19edc3e865b842cef77f4
cameronpm1 <cameronpm99@gmail.com>
2023-11-07 14:07:42 -0500

    ryans updates

--- envs/drone_env.py
+++ envs/drone_env.py
@@ -4,10 +4,13 @@ sys.path.insert(1, 'c:/Users/Cameron Mehlman/Documents/magpie hardware')
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-from pymavlink import mavutil
 from typing import Any, Dict, Type, Optional, Union
 
 from trajectory_planning.path_planner import pathPlanner
+
+import asyncio
+from mavsdk import System
+from mavsdk.offboard import (OffboardError, PositionNedYaw)
 
 class magpieEnv():
 
@@ -29,7 +32,7 @@ class magpieEnv():
         self.global_path = None
         self.rel_path = None
 
-        self.connection = None
+        self.drone = System()
 
     
 
@@ -102,7 +105,7 @@ class magpieEnv():
     '''
     SUPPORT FUNCTIONS TO COMMUNICATE BETWEEN FLIGHT CONTROLLER AND DRONE ENV
     '''
-
+    '''
     def update_state(self) -> None:
         '''
         get current positional/velocity state from pixhawk controller
@@ -119,39 +122,147 @@ class magpieEnv():
         '''
         pass
 
-    def initialize_connection(
+    async def initialize_connection(
             self,
             port: str = '/dev/ttyTHS1',
             baud_rate: int = 57600,
     ) -> None:
-        self.connection = mavutil.mavlink_connection(serial_port, baud=baud_rate)
+        
+        await self.drone.connect(system_address="port", baud=baud_rate)
 
-    def check_connection(
+        status_text_task = asyncio.ensure_future(print_status_text(self.drone))
+
+        print("Waiting for connection...")
+        async for state in self.drone.core.connection_state():
+            if state.is_connected:
+                print("Connection successful")
+                break
+
+    async def print_status_text(self):
+        try:
+            async for status_text in self.drone.telemetry.status_text():
+                print(f"Status: {status_text.type}: {status_text.text}")
+        except asyncio.CancelledError:
+            return
+
+    async def check_connection(
             self,
     ) -> bool:
-        heartbeat = mavutil.mavlink.MAVLink_heartbeat_message(6, 8, 192, 0, 4, 0)
-        self.connection.mav.send(heartbeat)
-        print("Sent heartbeat")
+        
+        print("Waiting for global position estimate...")
+        async for health in self.drone.telemetry.health():
+            if health.is_global_position_ok and health.is_home_position_ok:
+                print("Global position estimate OK")
+                break
+        
+        print("Arming")
+        await self.drone.action.arm()
 
         return True
+    '''
+
+    async def print_status_text():
+        drone = System()
+        await drone.connect(system_address="udp://:14540")
+
+        print("Waiting for drone to connect...")
+        async for state in drone.core.connection_state():
+            if state.is_connected:
+                print(f"-- Connected to drone!")
+                break
+
+        async for status_text in drone.telemetry.status_text():
+            print("Statustext:", status_text)
+
+        async for battery in drone.telemetry.battery():
+            print(f"Battery: {battery.remaining_percent}")
+            
+    async def perform_connection(drone):
+        await drone.connect(system_address="udp://:14540")
+
+    async def perform_calibrations(drone):
+        print("-- Starting gyroscope calibration")
+        async for progress_data in drone.calibration.calibrate_gyro():
+            print(progress_data)
+        print("-- Gyroscope calibration finished")
+
+        print("-- Starting accelerometer calibration")
+        async for progress_data in drone.calibration.calibrate_accelerometer():
+            print(progress_data)
+        print("-- Accelerometer calibration finished")
+
+        print("-- Starting magnetometer calibration")
+        async for progress_data in drone.calibration.calibrate_magnetometer():
+            print(progress_data)
+        print("-- Magnetometer calibration finished")
+
+        print("-- Starting board level horizon calibration")
+        async for progress_data in drone.calibration.calibrate_level_horizon():
+            print(progress_data)
+        print("-- Board level calibration finished")
+    
+    async def print_gps_info(drone):
+        async for gps_info in drone.telemetry.gps_info():
+            print(f"GPS info: {gps_info}")
+
+
+    async def print_in_air(drone):
+        async for in_air in drone.telemetry.in_air():
+            print(f"In air: {in_air}")
+    
+    async def print_position(drone):
+        async for position in drone.telemetry.position():
+            print(position)
+
+    async def set_new_position(drone,NED):
+        print("-- Arming")
+        await drone.action.arm()
+
+        print("-- Setting initial setpoint")
+        await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, 0.0, 0.0))
+
+        print("-- Starting offboard")
+        try:
+            await drone.offboard.start()
+        except OffboardError as error:
+            print(f"Starting offboard mode failed \
+                    with error code: {error._result.result}")
+            print("-- Disarming")
+            await drone.action.disarm()
+            return
+
+        print("-- Go 0m North, 0m East, -5m Down \
+                within local coordinate system")
+        await drone.offboard.set_position_ned(
+                PositionNedYaw(NED[1], NED[2], NED[3], NED[4]))
+        await asyncio.sleep(10)
+
+        print("-- Stopping offboard")
+        try:
+            await drone.offboard.stop()
+        except OffboardError as error:
+            print(f"Stopping offboard mode failed \
+                    with error code: {error._result.result}")
+
+    async def takeoff(drone):
+                    # Execute the maneuvers
+        print("-- Arming")
+        await drone.action.arm()
+
+        print("-- Taking off")
+        await drone.action.set_takeoff_altitude(10.0)
+        await drone.action.takeoff()
+
+        await asyncio.sleep(10)
+
+    async def land(drone):
+        print("-- Landing")
+        await drone.action.land()
 
 
 if __name__ == "__main__": 
-    mavu = mavutil.mavlink_connection(ser)
-
-    # Define the serial port and baud rate
-    serial_port = '/dev/ttyTHS1' # Jetson Nano's serial port
-    baud_rate = 57600 # Match the TELEM2 port's baud rate
-    # Initialize the serial connection
-    connection = mavutil.mavlink_connection(serial_port, baud=baud_rate)
-    print("done")
-    # Send a heartbeat message
-    heartbeat = mavutil.mavlink.MAVLink_heartbeat_message(6, 8, 192, 0, 4, 0)
-    connection.mav.send(heartbeat)
-    print("Sent heartbeat")
-    # Close the serial connection when done
-    connection.close()
-    print("finished")
+    #await drone.offboard.set_position_ned(PositionNedYaw(0.0, 0.0, 0.0, 0.0))
+    pass
 
 
 
